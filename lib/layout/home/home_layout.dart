ad5077bb41f115e7beeb19c773c4e0740a0cd5ed
alaahosny9 <alaahosnii1000@gmail.com>
2023-06-11 20:09:32 +0300

    initial commit

---
+++ lib/layout/home/home_layout.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomeLayout extends StatelessWidget {
+  static String routeName = "Home";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
